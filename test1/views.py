dbd0c81a16c2c55d4da3f4ce28ab72d1b819e503
notepad <12334443@qq.com>
2018-03-17 21:33:07 +0800

    0317update

--- test1/views.py
+++ test1/views.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from django.shortcuts import render
-from test1.models import classstatus
+from .models import classstatus,otsteam
 
 def home(request):
- #   for i in classstatus.objects.all():
-# Ccount = classstatus.objects.count()
- #       print(classstatus.ClassName')
+# UsePer = classstatus.objects.filter(ClassStatus=1).count() / classstatus.objects.all().count()
+    isum = classstatus.objects.filter(ClassStatus=1).count() / classstatus.objects.all().count()
+    isum = round(isum * 100)
+    isum100 = 100-isum
 
-    return render(request,'index.html',context={'Cstatus':classstatus.objects.all(),'Ccount1':classstatus.objects.filter(ClassStatus=1).count()})
+# UsePer = round(UsePer*100)
+# Ccount = classstatus.objects.count()
+    print(isum)
+    print(isum100)
+    print(otsteam.objects.all().count())
+
+    return render(request,'index.html',context={'Cstatus':classstatus.objects.all(),'Ccount1':classstatus.objects.filter(ClassStatus=1).count(),'CcountSum':classstatus.objects.all().count(),'isum':isum,'isum100':isum100,'otsteams':otsteam.objects.all()})
